81f4e1625699be98b95296c7020f079e96ec9ab2
ignizxl <igor.joao@academico.ifpb.edu.br>
2023-08-17 22:03:02 -0300

    up

--- JavascriptEssencial/aula06/src/script.js
+++ JavascriptEssencial/aula06/src/script.js
@@ -76,3 +76,26 @@ var idade = 18;
 //menor
 var situacao = (idade >= 18) ? "Maioridade":"Menor"; 
 console.log(situacao);
+
+
+//extra: switch case
+
+//a estrutura do switch case é bem simples, imagine cada case como um if seguido vários else if e o default como um else
+//exemplos
+var diaDaSemana = new Date().getDate(); //pegando o dia da semana e armazenando dentro de 'diaDaSemana'
+switch (diaDaSemana){
+
+    //se for igual a zero, hoje é domingo
+    //obs: ao fim de cada caso, coloque um break 
+    case 0:
+        console.log("Hoje é domingo. #CurandoARessaca, #ComeçandoACura");
+        break;
+        //se for igual a 6, hoje é sabádo
+    case 6:
+        console.log("Hoje é sabádo. #Curtição, #PartiuBalada");
+        break;
+    //senão, é um dia útil 
+    default:
+        console.log("Dia normal. #VaiEstudarVagabundo!");
+        break;
+}
